3fd4517eaeadd78fb1f59bd772d92f6b539a86a9
sonicpro <sonicpro@ukrpost.ua>
2017-08-04 15:45:20 +0300

    Exception handling code added.

--- faviconbrowser/TAP version/FaviconBrowser/MainWindow.xaml.cs
+++ faviconbrowser/TAP version/FaviconBrowser/MainWindow.xaml.cs
@@ -25,6 +25,8 @@ namespace FaviconBrowser
 																 //"facebook.com",
 																 //"twitter.com",
 																 //"reddit.com",
+																 "0",
+																 null,
                                                                  "baidu.com",
                                                                  "bbc.co.uk"
                                                              };
@@ -54,9 +56,21 @@ namespace FaviconBrowser
 		//	m_WrapPanel.Children.Add(imageControl);
 		//}
 
-		// We have an added advantage of collecting tasks in the calling code rather than voids as in the case of AddAFavicon() method.
-		// Therefore, no exception is retrown in the calling code if one of the tasks returned in the faulted state.
-		private async Task<Image> GetFavicon(string domain)
+		private Task<Image> GetFavicon(string domain)
+		{
+			int numeric;
+			if (domain == null)
+			{
+				throw new ArgumentNullException("domain");
+			}
+			else if (Int32.TryParse(domain, out numeric))
+			{
+				throw new ArgumentException("domain");
+			}
+			return GetFaviconInternal(domain);
+		}
+
+		private async Task<Image> GetFaviconInternal(string domain)
 		{
 			WebClient webClient = new WebClient();
 			byte[] bytes = await webClient.DownloadDataTaskAsync("http://" + domain + "/favicon.ico");
@@ -72,13 +86,29 @@ namespace FaviconBrowser
 		// Notice that no sooner than all the tasks that are put in the "results" task have finished, the foreach loop starts executing.
 		private async void GetButton_OnClick(object sender, RoutedEventArgs e)
 		{
-			Task<Image>[] tasks = s_Domains.Select(GetFavicon).ToArray();
+			// This line is for synchronous exception simulation demo.
+			//Task<Image>[] tasks = s_Domains.Select(GetFavicon).ToArray();
+
+			Task<Image>[] tasks = s_Domains.Select(GetFaviconInternal).ToArray();
 			// Create the task that aggregates several tasks by calling a Task.WhenAll(Task[]) combinator.
 			Task<Image[]> results = Task.WhenAll(tasks);
 			System.Diagnostics.Debug.Print(results.Status.ToString()); // Returns "WaitingForActivation".
-			foreach(var image in await results)
+			try
 			{
-				ShowFavicon(image);
+				foreach (var image in await results)
+				{
+					ShowFavicon(image);
+				}
+			}
+			catch (Exception ex)
+			{
+				// We can compare what exception is thrown on "await results" completion with the actual exceptions put to the
+				// Task<Image[]>.Exception.InnerExceptions list.
+				System.Diagnostics.Debug.Print("Thrown on await: {0}", ex);
+				foreach (Exception o in results.Exception.InnerExceptions)
+				{
+					System.Diagnostics.Debug.Print("Put in the inner exception list: {0}", o);
+				}
 			}
 		}
 
