ca356123eaad32cd31362b878ab7a1276047897d
sonicpro <sonicpro@ukrpost.ua>
2017-08-06 18:57:40 +0300

    TheAsyncCompilerTransform (in Depth) demo added.

---
+++ TheAsyncCompilerTransform/AlexsClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheAsyncCompilerTransform
+{
+	class AlexsClass
+	{
+		public async Task<int> AlexsMethod()
+		{
+			int foo = 3;
+			// We use non-generic Task in this demo.
+			// The stub method code contains the following lines:
+			// "awaiter = Task.Delay(500).GetAwaiter();"
+			// and then on case: 0 (async completion):
+			// "awaiter.GetResult();" - this line does not produce assigneble value,
+			// but re-throws an exception if one occurred during the asynchronous operation.
+			await Task.Delay(500);
+			return foo;
+		}
+	}
+}
---
+++ TheAsyncCompilerTransform/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nito.AsyncEx;
+
+namespace TheAsyncCompilerTransform
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				AsyncContext.Run(async () =>
+				{
+					var inst = new AlexsClass();
+					Console.WriteLine(await inst.AlexsMethod());
+				});
+				Console.ReadKey();
+			}
+			catch (Exception ex)
+			{
+				Console.Error.WriteLine(ex);
+			}
+		}
+	}
+}
